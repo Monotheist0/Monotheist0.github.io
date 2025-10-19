b35c724c69d3cf49bf3d0c89d43b9b1545f13cd6
Monotheist <masudrahman998@gmail.com>
2025-10-19 17:00:56 +0600

    pc scrollwheel had died gg fixed it

--- js/book.js
+++ js/book.js
@@ -45,17 +45,20 @@ function initBook() {
   loadProjects();
   // Add keyboard navigation
   document.addEventListener("keydown", handleKeyPress);
-  const projectsContainer = document.getElementById("projectsContainer");
-  if (projectsContainer) {
-    projectsContainer.addEventListener("wheel", function (e) {
-      // Check if the container is actually scrollable
-      const hasScrollbar = this.scrollHeight > this.clientHeight;
-      if (hasScrollbar) {
-        // Stop the scroll event from bubbling up to the body
-        e.stopPropagation();
-      }
-    });
-  }
+  const scrollableAreas = document.querySelectorAll(".projects-container, .skills-grid, .content");
+
+  const handleWheelScroll = function (e) {
+    // Check if the container is actually scrollable
+    const hasScrollbar = this.scrollHeight > this.clientHeight;
+    if (hasScrollbar) {
+      // Stop the scroll event from bubbling up to the body
+      e.stopPropagation();
+    }
+  };
+
+  scrollableAreas.forEach((area) => {
+    area.addEventListener("wheel", handleWheelScroll);
+  });
 }
 
 function playFlipSound() {
